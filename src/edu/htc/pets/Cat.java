e00088b770bfa0f9c4fc519002b5ce42c49d2d5e
jordovolk <jordovolk@gmail.com>
2016-02-06 12:10:16 -0600

    pet age calculator improved with objects

--- src/edu/htc/pets/Cat.java
+++ src/edu/htc/pets/Cat.java
@@ -8,6 +8,41 @@ package edu.htc.pets;
  * There is also a function to calculate their human age.
  */
 public class Cat {
+    private String catName;
+
+    private double catAge;
+
+    public String getName() {
+        return catName;
+    }
+
+    public void setName(String name) {
+        this.catName = name;
+    }
+
+    public double getAge() {
+        return catAge;
+    }
+
+    public void setAge(Double age) {
+        this.catAge = age;
+    }
+
+    public double calculateCatHumanAge() {
+        double humanAge = 0;
+        //1 year old cat = 15
+        if(catAge == 1) {
+            humanAge = 15; }
+        //2 year old cat is 25
+        else if(catAge == 2) {
+            humanAge = 25;
+        }
+        //every other year after 2 is + 4 years
+        else {
+            humanAge = 25 + 4 *(catAge-2);
+        }
+        return humanAge;
+    }
 
 
 
--- src/edu/htc/pets/Main.java
+++ src/edu/htc/pets/Main.java
@@ -3,7 +3,59 @@ package edu.htc.pets;
 public class Main {
 
     public static void main(String[] args) {
+        //cat array and info
+        Cat[] Catarray = new Cat[3];
+        Cat garfield = new Cat();
+        garfield.setAge(10.00);
+        garfield.setName("garfield");
+        Catarray[0] = garfield;
 
-        
+        Cat joe = new Cat();
+        joe.setAge(20.00);
+        joe.setName("joe");
+        Catarray[1] = joe;
+
+        Cat bill = new Cat();
+        bill.setAge(30.00);
+        bill.setName("bill");
+        Catarray[2] = bill;
+
+        System.out.println("Cat information: ");
+        for (int i = 0; i < Catarray.length; i++) {
+            Cat cat = Catarray[i];
+            System.out.print(cat.getName() + " is " + cat.getAge() + " and is " + cat.calculateCatHumanAge() + " human years old. ");
+            System.out.println();
+        }
+        System.out.print("\n");
+
+        //dog array and info
+        Dog[] Dogarray = new Dog[3];
+        Dog zoey = new Dog("zoey");
+        zoey.setAge(8.00);
+        Dogarray[0] = zoey;
+
+        Dog spiderdog = new Dog("spiderdog");
+        spiderdog.setAge(15.00);
+        Dogarray[1] = spiderdog;
+
+        Dog tanya = new Dog("tanya");
+        tanya.setAge(11.00);
+        Dogarray[2] = tanya;
+
+        //Catarray[3] = zoey; //incompatible type error, said looking for cat object and found dog
+
+        //Cat myCat = new Cat(); //yes you can create a cat without a name
+        //Dog myDog = new Dog();//no you cannot create a dog without a name
+
+        System.out.println("Dog information: ");
+        for (int i = 0; i < Dogarray.length; i++) {
+            Dog dog = Dogarray[i];
+            System.out.print(dog.getName() + " is " + dog.getAge() + " and is " + dog.calculateDogHumanAge() + " human years old. ");
+            System.out.println();
+
+
+        }
+        //what happens when you print an object
+        //System.out.println(spiderdog); //edu.htc.pets.Dog@a14482 prints when you print an object
     }
 }
