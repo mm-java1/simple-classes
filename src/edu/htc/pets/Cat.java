865ef8edb2b31072ea3a630c462a67dc0676b8e9
ggauchan001 <ggauchan001@users.noreply.github.com>
2016-02-08 20:50:24 -0600

    Simple-classes

--- src/edu/htc/pets/Cat.java
+++ src/edu/htc/pets/Cat.java
@@ -8,7 +8,43 @@ package edu.htc.pets;
  * There is also a function to calculate their human age.
  */
 public class Cat {
+    private String catName;
+    private double catAge;
 
+    public String getName(){
+        return catName;
+    }
+
+    public void setName(String catName){
+        this.catName = catName;
+    }
+
+    public double getAge(){
+        return catAge;
+    }
+
+    public void setAge(Double catAge){
+        this.catAge = catAge;
+    }
+
+    //Calculating cat age in human years
+    public double getCatHumanAge(){
+        double humanAge =0;
+
+        if (this.getAge() ==1){
+            humanAge = 15;
+        }
+        else if (this.getAge() == 2){
+            humanAge = 25;
+        }
+        else{
+            //first two years, cat will be 25 human year
+            humanAge = 25;
+            //every other year after is 4 human year
+            humanAge +=4 *(this.getAge() -2);
+        }
+        return humanAge;
+    }
 
 
 }
--- src/edu/htc/pets/Dog.java
+++ src/edu/htc/pets/Dog.java
@@ -8,6 +8,48 @@ package edu.htc.pets;
  * There is also a function to calculate their human age.
  */
 public class Dog {
+    private String dogNames;
+    private Double dogAge;
+
+    //Creating a constructor
+//    public Dog (String dogNames){
+//        this.dogNames = dogNames;
+//    }
+
+    public void setName(String dogNames){
+        this.dogNames = dogNames;
+    }
+    public String getName(){
+
+        return dogNames;
+    }
+
+    public void setAge(double dogAge) {
+        this.dogAge = dogAge;
+    }
+
+    public double getAge(){
+        return dogAge;
+    }
+
+
+
+
+    //Calculating dog age in human years
+    public double getDogHumanAge(){
+        double humanAge = 0;
+        if(this.getAge() <=2){
+            humanAge= this.getAge() * 10.5;
+        }else{
+            //first two years, gets you 21 humna years
+            humanAge = 21;
+            //every other year after is 4 human years
+            humanAge += 4 * (this.getAge() - 2);
+        }
+        return humanAge;
+
+    }
+
 
 
 }
