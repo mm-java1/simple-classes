b7f87edf9b3bd430b7bd373ecdafc7f427fbead0
Karen Nilson <Karennilson@live.com>
2016-02-20 21:10:52 -0600

    done

--- src/edu/htc/pets/Cat.java
+++ src/edu/htc/pets/Cat.java
@@ -8,7 +8,47 @@ package edu.htc.pets;
  * There is also a function to calculate their human age.
  */
 public class Cat {
+    private String catName;
+    private int catAge;
 
 
+    public String getCatName() {
+        return catName;
+    }
 
+    public void setCatName(String S){
+        catName = S;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "catName='" + catName + '\'' +
+                ", catAge=" + catAge +
+                '}';
+    }
+
+    public int getCatAge() {
+        return catAge;
+    }
+
+    public void setCatAge(int x) {
+        catAge = x;
+    }
+
+    public double calculateCatHumanAge() {
+        double humanAge = 0;
+
+        if (catAge <= 1) {
+            humanAge = 15;
+        } else if (catAge == 2) {
+            humanAge = 25;
+        } else {
+            humanAge = 25;
+
+            humanAge += 4 * (catAge - 2);
+        }
+
+        return humanAge;
+    }
 }
--- src/edu/htc/pets/Main.java
+++ src/edu/htc/pets/Main.java
@@ -3,7 +3,51 @@ package edu.htc.pets;
 public class Main {
 
     public static void main(String[] args) {
+        Cat[] cats=new Cat[3];
 
-        
+        Cat whiskers= new Cat();
+        System.out.println("showing nameless cats name "+ whiskers.getCatName());
+        whiskers.setCatName("whiskers");
+        whiskers.setCatAge(10);
+        cats[0]=whiskers;
+
+        Cat hugh= new Cat();
+        hugh.setCatName("hugh");
+        hugh.setCatAge(4);
+        cats[1]=hugh;
+
+        Cat laden= new Cat();
+        laden.setCatName("laden");
+        laden.setCatAge(2);
+        cats[2]=laden;
+
+        System.out.println("showing cats");
+        for(int i=0; i<cats.length; i++){
+            Cat currentCat=cats[i];
+            System.out.println("our cat is " + currentCat.getCatName() + " his age is " + currentCat.getCatAge() + " his human age is " + currentCat.calculateCatHumanAge());
+        }
+
+        Dog[] dogs=new Dog[3];
+
+        Dog spot=new Dog("spot");
+        spot.setDogAge(7);
+        dogs[0]=spot;
+
+        Dog spike=new Dog("spike");
+        spike.setDogAge(3);
+        dogs[1]=spike;
+
+        Dog ruby=new Dog("ruby");
+        ruby.setDogAge(1);
+        dogs[2]=ruby;
+
+        System.out.println("showing dogs");
+        for (Dog currentDog:dogs){
+            System.out.println("our dog is " + currentDog.getDogName() + " his age is " + currentDog.getDogAge() + " his human age is " + currentDog.calculateDogHumanAge());
+        }
+        System.out.println(whiskers);
+
+
+
     }
 }
